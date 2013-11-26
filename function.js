50e7b8e45b2fcf06d845c5d463088f2db0b80ae1
Bastian Gruber <coding@bastiangruber.de>
2013-11-26 21:47:08 +0100

    Create new project and add the first lines

---
+++ function.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+
+	for (var i = 5; i < 10; i++) {
+		//Irgendwas rein
+	};
+
+//	 $(".surveybutton").each(function() {
+ //       var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + 
+ //       	(Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
+ //        $(this).css("background-color", hue);
+ //   });
+
+
+});
