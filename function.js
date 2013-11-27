4685522a4f417274074ab67079c476107e0e53f8
Bastian Gruber <coding@bastiangruber.de>
2013-11-27 10:21:50 +0100

    added style, changed order in form (js)

--- function.js
+++ function.js
@@ -8,39 +8,52 @@ $(document).ready(function(){
   		});
 	});
 
-
-
-
-});
-
 //click on a button and get the survey from button-id
-$(".surveybutton").live('click', function() {
-     var id = $(this).attr("id");
-     var i = 0;
+	$(".surveybutton").live('click', function() {
+    	 var id = $(this).attr("id");
+    	 var i = 0;
 
-     $('.surveybutton').fadeOut(100).hide();
+    	 $('.surveybutton').fadeOut(100).hide();
 
-     $.getJSON( "http://surveysmock.apiary.io/api/surveys/" + id , function( single ) {
-     		$( "#content" ).append("<h2>" + single.survey.title + "</h2><p>" + single.survey.tagline + "</p>");
-     		$( "#content" ).append("<form class='surveyquestions' id='" + single.survey.id + "' action=''>");
- 			$.each( single.survey.questions, function( key, val ) {
- 				$( "form" ).append("<label class='questions' id='" + val.id + "'>" + val.title + "</label><br />");
+    	 $.getJSON( "http://surveysmock.apiary.io/api/surveys/" + id , function( single ) {
+    	 		$( "#content" ).append("<h2>" + single.survey.title + "</h2><p>" + single.survey.tagline + "</p>");
+     			$( "#content" ).append("<form class='surveyquestions' id='" + single.survey.id + "'>");
+ 				$.each( single.survey.questions, function( key, val ) {
+ 					$( "form" ).append("<label class='questions' id='" + val.id + "'>" + val.title + "</label><br />");
 
- 				$.each( val.options, function() {
- 					$( "label#" + val.id ).append("<input type='radio' name='" + val.id + "' value='" + val.options[i] + "'>" + val.options[i] + "<br />");
- 					i++;	
- 				});
+ 					$.each( val.options, function() {
+ 						$( "label#" + val.id ).append("<input type='radio' name='" + val.id + "' value='" + val.options[i] + "'>" + val.options[i] + "<br />");
+ 						i++;	
+ 					});
 
-				i = 0;
-	 		 });
+					i = 0;
+	 			 });
 
- 	 $( "#content" ).append("</form>");
-     $( "#content" ).append("<input type='button' value='Back' onClick='window.location.reload()'>");
-     $( "#content" ).append("<input type='submit' value='Submit'>");
+    	 $( "#content" ).append("<input type='button' value='Back' onClick='window.location.reload()'>");
+   		 $( "#content" ).append("<input type='submit' value='Submit'>");
+ 	 	$( "#content" ).append("</form>");
+
+		});
+
+ 	});
+
+
+	//submit the form and send it with post to server
+	$(".surveyquestions").live('submit',function() {
+		alert();
+		completion = [];
+
+		$("input[type='radio']:checked").each(function() {
+        	var questionID = $(this).attr("name");
+        	var radioValue = $(this).attr("value");
+
+        	item = {};
+        	item["questionID"] = questionID;
+        	item["radioValue"] = radioValue;
+
+        	completion.push(item);
+        
+    	});
+
 	});
-
- });
-
-
-//submit the form and send it with post to server
-
+});
